812fe307d16a380d4b8cbcbd2e302cfb57dcea29
sauce-chili <saucechilidev@gmail.com>
2024-09-29 23:20:23 +0300

    delete: spring test

--- src/test/java/vstu/isd/urlshortenerservice/UrlShortenerApplicationTests.java
+++ src/test/java/vstu/isd/urlshortenerservice/UrlShortenerApplicationTests.java
@@ -1,13 +1,12 @@
 package vstu.isd.urlshortenerservice;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UrlShortenerApplicationTests {
-
     @Test
     void contextLoads() {
+        Assertions.assertThat(40 + 2).isEqualTo(42);
     }
-
 }

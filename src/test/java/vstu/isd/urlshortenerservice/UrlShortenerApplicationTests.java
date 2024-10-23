6489f056f99b185f0fd0f4d1ebf08df0aa684daa
Vacobi <roman.yurasov.v@gmail.com>
2024-10-23 22:43:06 +0300

    refactor: Deleted private fields from UrlShortenerApplicationTests class

--- src/test/java/vstu/isd/urlshortenerservice/UrlShortenerApplicationTests.java
+++ src/test/java/vstu/isd/urlshortenerservice/UrlShortenerApplicationTests.java
@@ -4,9 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class UrlShortenerApplicationTests {
-    private final int minIndex = 0;
-    private final int maxIndex = 62;
-
     @Test
     void contextLoads() {
         Assertions.assertThat(40 + 2).isEqualTo(42);
